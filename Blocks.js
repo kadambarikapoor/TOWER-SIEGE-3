f386d4621e91a5d3281ad217cb040f9aa1b3cfe7
kadambarikapoor <66360446+kadambarikapoor@users.noreply.github.com>
2020-08-10 08:22:08 +0530

    Add files via upload

---
+++ Blocks.js
@@ -0,0 +1,39 @@
+class Block{
+    constructor(x,y,width, height){
+        var options={
+            isStatic:false
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.Visiblity = 255;
+        World.add(world, this.body);
+    }
+
+    score(){
+        if (this.Visiblity < 0 && this.Visiblity >-105){
+            score++;
+        }
+    }
+    display(){
+        if(this.body.speed < 3){
+            var angle = this.body.angle;
+            push();
+            translate(this.body.position.x, this.body.position.y);
+            rotate(angle);
+            rectMode(CENTER);
+            fill("pink")
+            rect(0, 0, this.width, this.height);
+            pop();
+           }
+           else{
+             World.remove(world, this.body);
+             push();
+             this.Visiblity = this.Visiblity - 5;
+             //tint(255,this.Visiblity);
+             pop();
+           }
+          }
+    }
